ed710f7316801d0877ec0c625adacc750c31e541
NIHILncunia <nihil_ncunia@naver.com>
2024-01-30 18:27:51 +0900

    2024 0130 결과 일부 업데이트

--- src/commands/preset.js
+++ src/commands/preset.js
@@ -26,7 +26,7 @@ module.exports = {
       .setColor('Red')
       .setFields(...result.map((item) => ({
         name: item.formula,
-        value: `- 전체 결과: **[ ${item.diceTotal} ]**\n- 상세 결과: ${item.diceDetails.map((item2) => `${item2.dice} **[ ${item2.total} ]** (${item2.details.join(',')})\n`)}- 보정: [${item.modDetails.join(',')}]`,
+        value: `- 전체 결과: **[ ${item.diceTotal} ]**\n**상세 결과:**\n${item.diceDetails.map((item2) => `- ${item2.dice} **[ ${item2.total} ]** (${item2.details.join('')})\n`)}- 보정: [${item.modDetails.join(',')}]`,
       })))
 
     interaction.reply({
